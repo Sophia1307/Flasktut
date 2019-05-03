f33c8f0b087a928dd468f6e308c2664011b5e77c
Sophia1307 <48214263+Sophia1307@users.noreply.github.com>
2019-05-03 10:30:11 +0200

    Ch2 Templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,26 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "<h1>Moja prva Python aplikacija!</h1>"
+	user = {'username':'Sophia'}
+	posts = [
+		{
+			'author': {'username': 'Pero'},
+			'body': 'It is a sunny day here in Rijeka!'
+		},
+		{
+			'author': {'username': 'Ivo'},
+			'body': 'Za vikend je prognoza i dalje sunčano!'
+		},
+		{
+			'author': {'username': 'Marica'},
+			'body': 'Tko želi ići na izlet?'
+		},
+		{
+			'author': {'username': 'Maro'},
+			'body': 'Imam tri slobodna mjesta u autu!'
+		}
+		]
+	return render_template('index.html', title='A blog', user=user, posts=posts)
