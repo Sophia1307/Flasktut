62f4abf57d9178a8bc7835556da69ea913681791
Sophia1307 <48214263+Sophia1307@users.noreply.github.com>
2019-05-03 12:17:54 +0200

    receiving form data
    ch2 na pola

--- app/routes.py
+++ app/routes.py
@@ -1,26 +1,42 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-	user = {'username':'Sophia'}
-	posts = [
-		{
-			'author': {'username': 'Pero'},
-			'body': 'It is a sunny day here in Rijeka!'
-		},
-		{
-			'author': {'username': 'Ivo'},
-			'body': 'Za vikend je prognoza i dalje sunčano!'
-		},
-		{
-			'author': {'username': 'Marica'},
-			'body': 'Tko želi ići na izlet?'
-		},
-		{
-			'author': {'username': 'Maro'},
-			'body': 'Imam tri slobodna mjesta u autu!'
-		}
-		]
-	return render_template('index.html', title='A blog', user=user, posts=posts)
+    user = {'username': 'Sophia'}
+    posts = [
+        {
+            'author': {'username': 'Peter'},
+            'body': 'It is a sunny day here in Rijeka!'
+        },
+        {
+            'author': {'username': 'John'},
+            'body': 'Will be sunny on weekend!'
+        },
+        {
+            'author': {'username': 'Mary'},
+            'body': 'Who wants to go for a walk?'
+        },
+        {
+            'author': {'username': 'Paul'},
+            'body': 'I am in!'
+        }
+    ]
+    return render_template(
+        'index.html',
+        title='A blog',
+        user=user,
+        posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
